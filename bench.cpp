f6e51f63735e6314d9a8de7bda3aa6b26d2c9212
jsteemann <jan@arangodb.com>
2015-09-26 00:48:35 +0200

    added rapidjson for comparisons

--- bench.cpp
+++ bench.cpp
@@ -16,6 +16,10 @@
 #include "JasonSlice.h"
 #include "JasonType.h"
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 using Jason            = triagens::basics::Jason;
 using JasonBuffer      = triagens::basics::JasonBuffer;
 using JasonBuilder     = triagens::basics::JasonBuilder;
@@ -28,18 +32,36 @@ using JasonType        = triagens::basics::JasonType;
   
 using namespace std;
 
+static void usage () {
+  cout << "Usage: FILENAME.json RUNTIME_IN_SECONDS COPIES TYPE" << endl;
+  cout << "This program reads the file into a string, makes COPIES copies" << endl;
+  cout << "and then parses the copies in a round-robin fashion to Jason." << endl;
+  cout << "1 copy means its running in cache, more copies make it run" << endl;
+  cout << "out of cache. The target areas are also in a different memory" << endl;
+  cout << "area for each copy." << endl;
+  cout << "TYPE must be either 'jason' or 'rapidjson'." << endl;
+}
+
 int main (int argc, char* argv[]) {
   JasonSlice::Initialize();
 
-  if (argc < 4) {
-    cout << "Usage: FILENAME.json RUNTIME_IN_SECONDS COPIES" << endl;
-    cout << "This program reads the file into a string, makes COPIES copies" << endl;
-    cout << "and then parses the copies in a round-robin fashion to Jason." << endl;
-    cout << "1 copy means its running in cache, more copies make it run" << endl;
-    cout << "out of cache. The target areas are also in a different memory" << endl;
-    cout << "area for each copy." << endl;
-    return 0;
+  if (argc < 5) {
+    usage();
+    return EXIT_FAILURE;
   }
+
+  bool useJason;
+  if (strcmp(argv[4], "jason") == 0) {
+    useJason = true;
+  }
+  else if (strcmp(argv[4], "rapidjson") == 0) {
+    useJason = false;
+  }
+  else {
+    usage();
+    return EXIT_FAILURE;
+  }
+    
 
   size_t copies = stoul(argv[3]);
   int runTime = stoi(argv[2]);
@@ -73,9 +95,16 @@ int main (int argc, char* argv[]) {
   size_t total = 0;
   auto start = chrono::high_resolution_clock::now();
   decltype(start) now;
+
   do {
     for (int i = 0; i < 2; i++) {
-      outputs[count]->parse(inputs[count]);
+      if (useJason) {
+        outputs[count]->parse(inputs[count]);
+      }
+      else {
+        rapidjson::Document d;
+        d.Parse(inputs[count].c_str());
+      }
       count++;
       if (count >= copies) {
         count = 0;
@@ -88,7 +117,7 @@ int main (int argc, char* argv[]) {
   chrono::duration<double> totalTime 
       = chrono::duration_cast<chrono::duration<double>>(now - start);
   cout << "Total runtime: " << totalTime.count() << " s" << endl;
-  cout << "Have parsed " << total << " times using " << copies
+  cout << "Have parsed " << total << " times with " << argv[4] << " using " << copies
        << " copies of JSON data, each of size " << inputs[0].size() << "."
        << endl;
   cout << "Parsed " << inputs[0].size() * total << " bytes in total." << endl;
@@ -96,5 +125,5 @@ int main (int argc, char* argv[]) {
                         totalTime.count() << " bytes/s"
        << " or " << total / totalTime.count() 
        << " JSON docs per second." << endl;
-  return 0;
+  return EXIT_SUCCESS;
 }
