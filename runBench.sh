4cd116b9e2e489d03cba43c660a244fb8adedf66
jsteemann <jan@arangodb.com>
2015-09-28 19:57:24 +0200

    run make from runBench.sh

--- runBench.sh
+++ runBench.sh
@@ -1,29 +1,29 @@
-make
+make bench
 DURATION=10
 
 echo
-echo "small.json #######################"
+echo "# small.json #######################"
 echo -n "jason:     "
 ./bench jsonSample/small.json $DURATION 1 jason | grep "docs per second" 
 echo -n "rapidjson: "
 ./bench jsonSample/small.json $DURATION 1 rapidjson | grep "docs per second"
 
 echo
-echo "sample.json ######################"
+echo "# sample.json ######################"
 echo -n "jason:     "
 ./bench jsonSample/sample.json $DURATION 1 jason | grep "docs per second"
 echo -n "rapidjson: "
 ./bench jsonSample/sample.json $DURATION 1 rapidjson | grep "docs per second"
 
 echo
-echo "sampleNoWhite.json ###############"
+echo "# sampleNoWhite.json ###############"
 echo -n "jason:     "
 ./bench jsonSample/sampleNoWhite.json $DURATION 1 jason | grep "docs per second"
 echo -n "rapidjson: "
 ./bench jsonSample/sampleNoWhite.json $DURATION 1 rapidjson | grep "docs per second"
 
 echo
-echo "commits.json #####################"
+echo "# commits.json #####################"
 echo -n "jason:     "
 ./bench jsonSample/commits.json $DURATION 1 jason | grep "docs per second"
 echo -n "rapidjson: "
