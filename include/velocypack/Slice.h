2889fa7ae9219aba1ca351f51d89f0692f57d50c
Jan Steemann <jan@arangodb.com>
2015-11-09 10:00:37 +0100

    removed useless default operations

--- include/velocypack/Slice.h
+++ include/velocypack/Slice.h
@@ -69,15 +69,6 @@ namespace arangodb {
           : _start(reinterpret_cast<uint8_t const*>(start)) {
         }
 
-        Slice (Slice const& other)
-          : _start(other._start) {
-        }
-
-        Slice& operator= (Slice const& other) {
-          _start = other._start;
-          return *this;
-        }
-
         // No destructor, does not take part in memory management,
         // standard copy, and move constructors, behaves like a pointer.
 
