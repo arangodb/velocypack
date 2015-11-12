d2e0632beef0eab835b1288f5ab84c9c4f470226
jsteemann <jan@arangodb.com>
2015-11-12 22:18:55 +0100

    added `--pretty` option for vpack-to-json

--- tools/vpack-to-json.cpp
+++ tools/vpack-to-json.cpp
@@ -33,31 +33,72 @@
 using namespace arangodb::velocypack;
 
 static void usage (char* argv[]) {
-  std::cout << "Usage: " << argv[0] << " INFILE OUTFILE" << std::endl;
+  std::cout << "Usage: " << argv[0] << " [OPTIONS] INFILE OUTFILE" << std::endl;
   std::cout << "This program reads the VPack INFILE into a string and saves its" << std::endl;
   std::cout << "JSON representation in file OUTFILE. Will work only for input" << std::endl;
   std::cout << "files up to 2 GB size." << std::endl;
+  std::cout << "Available options are:" << std::endl;
+  std::cout << " --pretty        pretty print JSON output" << std::endl;
 }
 
 int main (int argc, char* argv[]) {
-  if (argc < 3) {
+  char const* infileName = nullptr;
+  char const* outfileName = nullptr;
+  bool pretty = false;
+
+  int i = 1;
+  while (i < argc) {
+    char const* p = argv[i];
+    if (strncmp("--pretty", p, strlen("--pretty")) == 0) {
+      pretty = true;
+    }
+    else if (infileName == nullptr) {
+      infileName = p;
+    }
+    else if (outfileName == nullptr) {
+      outfileName = p;
+    }
+    else {
+      usage(argv);
+      return EXIT_FAILURE;
+    }
+    ++i;
+  }
+
+  if (infileName == nullptr) {
     usage(argv);
     return EXIT_FAILURE;
   }
 
+#ifdef __linux__
+  // treat missing outfile as stdout
+  if (outfileName == nullptr) {
+    outfileName = "/proc/self/fd/1";
+  }
+#else 
+  if (outfileName == nullptr) {
+    usage(argv);
+    return EXIT_FAILURE;
+  }
+#endif
+
   // treat "-" as stdin
-  std::string infile = argv[1];
+  std::string infile = infileName;
 #ifdef __linux__
   if (infile == "-") {
     infile = "/proc/self/fd/0";
   }
 #endif
+  if (argc < 3) {
+    usage(argv);
+    return EXIT_FAILURE;
+  }
 
   std::string s;
   std::ifstream ifs(infile, std::ifstream::in);
 
   if (! ifs.is_open()) {
-    std::cerr << "Cannot read infile '" << argv[1] << "'" << std::endl;
+    std::cerr << "Cannot read infile '" << infile << "'" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -69,25 +110,29 @@ int main (int argc, char* argv[]) {
   ifs.close();
 
   Slice const slice(s.c_str());
+
+  Options options;
+  options.prettyPrint = pretty;
+
   CharBufferSink sink(4096);
-  Dumper dumper(&sink);
+  Dumper dumper(&sink, &options);
 
   try {
     dumper.dump(slice);
   }
   catch (Exception const& ex) {
-    std::cerr << "An exception occurred while parsing infile '" << argv[1] << "': " << ex.what() << std::endl;
+    std::cerr << "An exception occurred while processing infile '" << infile << "': " << ex.what() << std::endl;
     return EXIT_FAILURE;
   }
   catch (...) {
-    std::cerr << "An unknown exception occurred while parsing infile '" << argv[1] << "'" << std::endl;
+    std::cerr << "An unknown exception occurred while processing infile '" << infile << "'" << std::endl;
     return EXIT_FAILURE;
   }
   
-  std::ofstream ofs(argv[2], std::ofstream::out);
+  std::ofstream ofs(outfileName, std::ofstream::out);
  
   if (! ofs.is_open()) {
-    std::cerr << "Cannot write outfile '" << argv[2] << "'" << std::endl;
+    std::cerr << "Cannot write outfile '" << outfileName << "'" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -99,14 +144,14 @@ int main (int argc, char* argv[]) {
   ofs.write(start, sink.buffer.size());
 
   if (! ofs) {
-    std::cerr << "Cannot write outfile '" << argv[2] << "'" << std::endl;
+    std::cerr << "Cannot write outfile '" << outfileName << "'" << std::endl;
     ofs.close();
     return EXIT_FAILURE;
   }
 
   ofs.close();
 
-  std::cout << "Successfully converted JSON infile '" << argv[1] << "'" << std::endl;
+  std::cout << "Successfully converted JSON infile '" << infile << "'" << std::endl;
   std::cout << "VPack Infile size: " << s.size() << std::endl;
   std::cout << "JSON Outfile size: " << sink.buffer.size() << std::endl;
   
