b93497a0116b8209e825dd12ec13da48f90b8a6a
jsteemann <jan@arangodb.com>
2015-11-19 23:08:20 +0100

    minor function improvements

--- include/velocypack/Slice.h
+++ include/velocypack/Slice.h
@@ -719,24 +719,6 @@ namespace arangodb {
           VELOCYPACK_ASSERT(this->type() == type);
         }
 #endif
-          
-        // read an unsigned little endian integer value of the
-        // specified length, starting at the specified byte offset
-        template <typename T>
-        T readInteger (uint8_t const* start, ValueLength numBytes) const {
-          T value = 0;
-          uint8_t const* p = start;
-          uint8_t const* e = p + numBytes;
-          T digit = 0;
-
-          while (p < e) {
-            value += static_cast<T>(*p) << (digit * 8);
-            ++digit;
-            ++p;
-          }
-
-          return value;
-        }
 
         // extracts a value from the slice and converts it into a 
         // built-in type
