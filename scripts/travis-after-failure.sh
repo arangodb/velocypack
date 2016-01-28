1d4aaa84cdb0949eac32c368d2c93bd245a1d059
jsteemann <jan@arangodb.com>
2016-01-29 00:54:10 +0100

    core files not supported in containerized builds

--- scripts/travis-after-failure.sh
+++
@@ -1,4 +0,0 @@
-COREFILE=$(find . -maxdepth 1 -name "core*" | head -n 1)
-if [[ -f "$COREFILE" ]]; then 
-  gdb -c "$COREFILE" bin/arangod -ex "thread apply all bt" -ex "set pagination 0" -batch || echo "no gdb found" 
-fi 
--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,4 +1,3 @@
-ulimit -c unlimited -S # enable core files
 mkdir -p build
 (cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCoverage=OFF -DBuildTests=ON -DBuildLargeTests=OFF -DBuildExamples=ON -DBuildTools=ON -DEnableSSE=OFF -DCMAKE_CXX_COMPILER=$CXX ..)
 (cd build && make)
