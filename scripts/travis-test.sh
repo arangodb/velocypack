d8c3ffdf82d4a8ae65227a926a20ae670c9155e0
Max Neunhoeffer <max@arangodb.com>
2016-02-22 15:47:51 +0100

    Add a travis test.

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,3 +2,5 @@ mkdir -p build
 (cd build && cmake -DCMAKE_BUILD_TYPE=Release -DCoverage=OFF -DBuildTests=ON -DBuildLargeTests=OFF -DBuildExamples=ON -DBuildTools=ON -DEnableSSE=OFF -DCMAKE_CXX_COMPILER=$CXX ..)
 (cd build && make)
 (cd build/tests && ctest -V)
+sleep 2
+echo Done.
