4da2ef0319620f8d7d374b987cc8b5d6fedc9975
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-25 09:13:18 +0200

    update travis

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -1,24 +1,17 @@
-if [ "$CXX" = "/usr/bin/g++-4.9" ]; then
+if [ "$CXX" = "g++-7" ]; then
   gem install coveralls-lcov
-  # install lcov 1.11
-  wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
-  tar xf lcov_1.11.orig.tar.gz
-  make -C lcov-1.11/ install PREFIX=~/bin/lcov
+  cd build
 
-  rm -Rf build/*
-  # build again
-  (cd build && cmake -DCoverage=ON -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=OFF -DBuildTools=OFF -DEnableSSE=OFF -DCMAKE_CXX_COMPILER=$CXX ..)
-  (cd build && make)
   # clear counters
-  ~/bin/lcov/usr/bin/lcov --capture --initial --directory build --output-file build/base_coverage.info --gcov-tool=gcov-4.9
-  ~/bin/lcov/usr/bin/lcov --directory build --zerocounters --gcov-tool=gcov-4.9
+  lcov --capture --initial --directory . --output-file base_coverage.info
+  lcov --directory . --zerocounters
   # run tests
-  (cd build/tests/ && ctest -V)
+  ctest -V
   # collect coverage info
-  ~/bin/lcov/usr/bin/lcov --directory build --capture --output-file build/test_coverage.info --gcov-tool=gcov-4.9
-  ~/bin/lcov/usr/bin/lcov --add-tracefile build/base_coverage.info --add-tracefile build/test_coverage.info --output-file build/coverage.info --gcov-tool=gcov-4.9
-  ~/bin/lcov/usr/bin/lcov --remove build/coverage.info 'tests/*' '/usr/*' 'src/*hash*' 'src/powers.h' --output-file build/coverage.info --gcov-tool=gcov-4.9
-  ~/bin/lcov/usr/bin/lcov --list build/coverage.info --gcov-tool=gcov-4.9
+  lcov --directory . --capture --output-file test_coverage.info
+  lcov --add-tracefile base_coverage.info --add-tracefile test_coverage.info --output-file coverage.info
+  lcov --remove coverage.info 'tests/*' '/usr/*' 'src/*hash*' 'src/powers.h' --output-file coverage.info
+  lcov --list coverage.info
   # upload coverage info
-  coveralls-lcov --repo-token ${COVERALLS_TOKEN} build/coverage.info
+  coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
 fi
--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,6 +1,31 @@
-mkdir -p build
-(cd build && cmake -DCMAKE_BUILD_TYPE=Release -DHashType=xxhash -DCoverage=OFF -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON -DBuildTools=ON -DEnableSSE=OFF -DCMAKE_CXX_COMPILER=$CXX ..)
-(cd build && make)
-(cd build/tests && ctest -V)
+#!/bin/bash
+
+ferr(){
+    echo "$@"
+    exit 1
+}
+
+${CXX_STANDARD:=14}
+${COVERAGE:=OFF}
+
+${BUILD_TYPE:=Release}
+if [[ $COVERAGE == "ON" ]]; then
+    BUILD_TYPE=Debug
+fi
+
+echo "Building with C++ Standard $CXX_STANDARD"
+echo "coverage: $COVERAGE"
+
+mkdir -p build && cd build || ferr "could not create build dir"
+
+cmake -DCMAKE_BUILD_TYPE=Release -DHashType=xxhash -DCoverage=${COVERAGE} \
+      -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON \
+      -DBuildTools=ON -DEnableSSE=OFF \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_CXX_STANDARD=${CXX_STANDARD}
+      .. || ferr "failed to configure"
+
+make || ferr "failed to build"
+ctest -V || ferr "failed to run tests"
 sleep 2
 echo Done.
