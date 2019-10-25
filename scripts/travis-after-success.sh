e9c8fc32740991f39acd8279885476ea73620dad
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-25 13:40:23 +0200

    try to get rid of absoulte path on coverage web-page

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -12,33 +12,34 @@ if ${COVERAGE:-false}; then
 
   echo "project directory $project_dir"
   echo "build directory $build_dir"
-  cd ${build_dir} || ferr "can not enter build dir"
-
+  cd ${project_dir} || ferr "can not enter build dir"
   gem install coveralls-lcov || ferr "failed to install gem"
 
+  LCOV=('lcov' '--directory' "$project_dir")
+
   # clear counters
-  lcov --directory "$build_dir" --capture --initial --output-file base_coverage.info || ferr "failed lcov"
-  lcov --directory "$build_dir" --zerocounters || ferr "failed lcov"
+  "${LCOV[@]}" --capture --initial --output-file base_coverage.info || ferr "failed lcov"
+  "${LCOV[@]}" --zerocounters || ferr "failed lcov"
 
   # run tests
-  (cd tests && ctest -V) || ferr "failed to run tests"
+  (cd "${build_dir}/tests" && ctest -V) || ferr "failed to run tests"
 
   # collect coverage info
-  lcov --directory "$build_dir" --capture --output-file test_coverage.info || ferr "failed lcov"
-  lcov --add-tracefile base_coverage.info --add-tracefile test_coverage.info --output-file coverage.info || ferr "failed lcov"
-  lcov --remove coverage.info \
-           '/usr/*' \
-           '*CMakeFiles/*' \
-           "$project_dir"'/examples/*' \
-           "$project_dir"'/tools/*' \
-           "$project_dir"'/tests/*' \
-           "$project_dir"'/src/*hash*' \
-           "$project_dir"'/src/powers.h' \
-            --output-file coverage.info || ferr "failed lcov"
+  "${LCOV[@]}" --capture --output-file test_coverage.info || ferr "failed lcov"
+  "${LCOV[@]}" --add-tracefile base_coverage.info --add-tracefile test_coverage.info --output-file coverage.info || ferr "failed lcov"
+  "${LCOV[@]}" --remove coverage.info \
+               '/usr/*' \
+               '*CMakeFiles/*' \
+               "$project_dir"'/examples/*' \
+               "$project_dir"'/tools/*' \
+               "$project_dir"'/tests/*' \
+               "$project_dir"'/src/*hash*' \
+               "$project_dir"'/src/powers.h' \
+                --output-file coverage.info || ferr "failed lcov"
+
+  "${LCOV[@]}" --list coverage.info || ferr "failed lcov"
 
   sed -i "s#${project_dir}/##" coverage.info
-  lcov --list coverage.info || ferr "failed lcov"
-
   # upload coverage info
   if ${COVERALLS_TOKEN:-false}; then
     coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info || ferr "failed to upload"
