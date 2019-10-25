14c6d4a211540be07c6c8e87bba1253f76783ba6
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-25 11:20:38 +0200

    change lcov base_dir

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -6,18 +6,19 @@ ferr(){
 }
 
 if ${COVERAGE:-false}; then
+  base_dir=".."
   gem install coveralls-lcov || ferr "failed to install gem"
   cd build
 
   # clear counters
-  lcov --capture --initial --directory . --output-file base_coverage.info || ferr "failed lcov"
-  lcov --directory . --zerocounters || ferr "failed lcov"
+  lcov --directory "$base_dir" --capture --initial --output-file base_coverage.info || ferr "failed lcov"
+  lcov --directory "$base_dir" --zerocounters || ferr "failed lcov"
 
   # run tests
   (cd tests && ctest -V) || ferr "failed to run tests"
 
   # collect coverage info
-  lcov --directory . --capture --output-file test_coverage.info || ferr "failed lcov"
+  lcov --directory "$base_dir" --capture --output-file test_coverage.info || ferr "failed lcov"
   lcov --add-tracefile base_coverage.info --add-tracefile test_coverage.info --output-file coverage.info || ferr "failed lcov"
   lcov --remove coverage.info 'tests/*' '/usr/*' 'src/*hash*' 'src/powers.h' --output-file coverage.info || ferr "failed lcov"
   lcov --list coverage.info || ferr "failed lcov"
