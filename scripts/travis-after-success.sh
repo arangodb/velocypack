30d1aaf889ef751ff412ac9ef903fa1191978784
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-25 11:52:16 +0200

    use full paths

--- scripts/travis-after-success.sh
+++ scripts/travis-after-success.sh
@@ -6,21 +6,29 @@ ferr(){
 }
 
 if ${COVERAGE:-false}; then
-  base_dir=".."
+  project_dir="$(readlink -f ..)"
+  build_dir="$(readlink -f .)"
+  echo "coverage base directory $build_dir"
+
   gem install coveralls-lcov || ferr "failed to install gem"
   cd build
 
   # clear counters
-  lcov --directory "$base_dir" --capture --initial --output-file base_coverage.info || ferr "failed lcov"
-  lcov --directory "$base_dir" --zerocounters || ferr "failed lcov"
+  lcov --directory "$build_dir" --capture --initial --output-file base_coverage.info || ferr "failed lcov"
+  lcov --directory "$build_dir" --zerocounters || ferr "failed lcov"
 
   # run tests
   (cd tests && ctest -V) || ferr "failed to run tests"
 
   # collect coverage info
-  lcov --directory "$base_dir" --capture --output-file test_coverage.info || ferr "failed lcov"
+  lcov --directory "$build_dir" --capture --output-file test_coverage.info || ferr "failed lcov"
   lcov --add-tracefile base_coverage.info --add-tracefile test_coverage.info --output-file coverage.info || ferr "failed lcov"
-  lcov --remove coverage.info 'tests/*' '/usr/*' 'src/*hash*' 'src/powers.h' --output-file coverage.info || ferr "failed lcov"
+  lcov --remove coverage.info \
+           "$project_dir"'tests/*' \
+           "$project_dir"'/usr/*' \
+           "$project_dir"'src/*hash*' \
+           "$project_dir"'src/powers.h' \
+            --output-file coverage.info || ferr "failed lcov"
   lcov --list coverage.info || ferr "failed lcov"
 
   # upload coverage info
