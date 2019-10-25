0ce7b399e475b302ff50d04dafbb6173270ff995
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-25 09:13:18 +0200

    enable build matrix and improve overall build

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,6 +1,39 @@
-mkdir -p build
-(cd build && cmake -DCMAKE_BUILD_TYPE=Release -DHashType=xxhash -DCoverage=OFF -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON -DBuildTools=ON -DEnableSSE=OFF -DCMAKE_CXX_COMPILER=$CXX ..)
-(cd build && make)
-(cd build/tests && ctest -V)
-sleep 2
+#!/bin/bash
+set -u
+ferr(){
+    echo "$@"
+    exit 1
+}
+
+
+CXX_STANDARD=${CXX_STANDARD:-14}
+echo "requesting c++ standard ${CXX_STANDARD}"
+COVERAGE=${COVERAGE:-OFF}
+BUILD_TYPE=${BUILD_TYPE:-Release}
+
+if [[ $COVERAGE == "ON" ]]; then
+    BUILD_TYPE=Debug
+fi
+
+threads=2
+if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+    threads=$(nproc)
+fi
+
+echo "Building with C++ Standard $CXX_STANDARD"
+echo "coverage: $COVERAGE"
+
+mkdir -p build && cd build || ferr "could not create build dir"
+
+cmake -DCMAKE_BUILD_TYPE=Release -DHashType=xxhash -DCoverage=${COVERAGE} \
+      -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON \
+      -DBuildTools=ON -DEnableSSE=OFF \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_CXX_STANDARD=${CXX_STANDARD} \
+      .. || ferr "failed to configure"
+
+make -j $threads || ferr "failed to build"
+if ! ${COVERAGE:-false}; then
+    (cd tests && ctest -V) || ferr "failed to run tests"
+fi
 echo Done.
