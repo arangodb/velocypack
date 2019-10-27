e028c5e0d6a36786bb6e07fc2e2a3784dddf0c5e
Jan Christoph Uhde <Jan@UhdeJC.com>
2019-10-27 13:31:43 +0100

    split test and build

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -5,34 +5,7 @@ ferr(){
     exit 1
 }
 
-
-CXX_STANDARD=${CXX_STANDARD:-14}
-echo "requesting c++ standard ${CXX_STANDARD}"
 COVERAGE=${COVERAGE:-OFF}
-BUILD_TYPE=${BUILD_TYPE:-Release}
-
-if [[ $COVERAGE == "ON" ]]; then
-    BUILD_TYPE=Debug
+if [[ $COVERAGE != OFF ]] ; then
+    (cd build/tests && ctest -V) || ferr "failed to run tests"
 fi
-
-threads=2
-if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    threads=$(nproc)
-fi
-
-echo "Building with C++ Standard $CXX_STANDARD"
-echo "coverage: $COVERAGE"
-
-mkdir -p build && cd build || ferr "could not create build dir"
-
-cmake -DCMAKE_BUILD_TYPE=Release -DHashType=xxhash -DCoverage=${COVERAGE} \
-      -DBuildTests=ON -DBuildLargeTests=OFF -DBuildVelocyPackExamples=ON \
-      -DBuildTools=ON -DEnableSSE=OFF \
-      -DCMAKE_CXX_STANDARD=${CXX_STANDARD} \
-      .. || ferr "failed to configure"
-
-make -j $threads || ferr "failed to build"
-if ! ${COVERAGE:-false}; then
-    (cd tests && ctest) || ferr "failed to run tests"
-fi
-echo Done.
