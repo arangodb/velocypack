568a03c0905457e4ec92e94979035402e7ac6a95
cpjulia <julia@arangodb.com>
2022-02-03 01:03:19 -0300

    Fully changed tabulation

--- tools/fuzzer.cpp
+++ tools/fuzzer.cpp
@@ -34,158 +34,158 @@
 using namespace arangodb::velocypack;
 
 enum class Format {
-    VPACK, JSON
+  VPACK, JSON
 };
 
 struct RandomGenerator {
-    RandomGenerator() : mt{rd()} {}
+  RandomGenerator() : mt{rd()} {}
 
-    std::random_device rd;
-    std::mt19937 mt;
+  std::random_device rd;
+  std::mt19937 mt;
 };
 
 RandomGenerator randomGenerator;
 
 static void usage(char* argv[]) {
-    std::cout << "Usage: " << argv[0] << " [OPTIONS] [ITERATIONS]"
-              << std::endl;
-    std::cout << "This program creates random VPack or JSON structures and validates them."
-              << std::endl;
-    std::cout << "The amout of times it does this is supplied by <iterations>."
-              << std::endl;
-    std::cout << "Available options are:" << std::endl;
-    std::cout << " --vpack       create VPack."
-              << std::endl;
-    std::cout << " --json        create JSON."
-              << std::endl;
-    std::cout << " <iterations>  number of iterations. Default: 1"
-              << std::endl;
+  std::cout << "Usage: " << argv[0] << " [OPTIONS] [ITERATIONS]"
+            << std::endl;
+  std::cout << "This program creates random VPack or JSON structures and validates them."
+            << std::endl;
+  std::cout << "The amout of times it does this is supplied by <iterations>."
+            << std::endl;
+  std::cout << "Available options are:" << std::endl;
+  std::cout << " --vpack       create VPack."
+            << std::endl;
+  std::cout << " --json        create JSON."
+            << std::endl;
+  std::cout << " <iterations>  number of iterations. Default: 1"
+            << std::endl;
 }
 
 static inline bool isOption(char const* arg, char const* expected) {
-    return (strcmp(arg, expected) == 0);
+  return (strcmp(arg, expected) == 0);
 }
 
 static void addString(Builder& builder) {
-    static auto &availableChars = "0123456789"
-                                  "abcdefghijklmnopqrstuvwxyz"
-                                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    size_t length = randomGenerator.mt() % 1000;
-    std::string s;
-    s.reserve(length);
-    while (length--) {
-        s += availableChars[randomGenerator.mt() % (sizeof(availableChars) - 1)];
-    }
-    builder.add(Value(s));
+  static auto &availableChars = "0123456789"
+                                "abcdefghijklmnopqrstuvwxyz"
+                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  size_t length = randomGenerator.mt() % 1000;
+  std::string s;
+  s.reserve(length);
+  while (length--) {
+    s += availableChars[randomGenerator.mt() % (sizeof(availableChars) - 1)];
+  }
+  builder.add(Value(s));
 }
 
 static void generateVelocypack(Builder& builder, size_t depth) {
-    while (true) {
-        size_t value = randomGenerator.mt() % 10;
-        if (depth > 10 && value < 2) {
-            continue;
+  while (true) {
+    size_t value = randomGenerator.mt() % 10;
+    if (depth > 10 && value < 2) {
+      continue;
+    }
+    switch (value) {
+      case 0: {
+        builder.openArray(randomGenerator.mt() % 2 ? true : false);
+        size_t numMembers = randomGenerator.mt() % 10;
+        for (size_t i = 0; i < numMembers; ++i) {
+          generateVelocypack(builder, depth + 1);
         }
-        switch (value) {
-            case 0: {
-                builder.openArray(randomGenerator.mt() % 2 ? true : false);
-                size_t numMembers = randomGenerator.mt() % 10;
-                for (size_t i = 0; i < numMembers; ++i) {
-                    generateVelocypack(builder, depth + 1);
-                }
-                builder.close();
-                break;
-            }
-            case 1: {
-                builder.openObject(randomGenerator.mt() % 2 ? true : false);
-                size_t numMembers = randomGenerator.mt() % 10;
-                for (size_t i = 0; i < numMembers; ++i) {
-                    std::string key = "test" + std::to_string(i);
-                    builder.add(Value(key));
-                    generateVelocypack(builder, depth + 1);
-                }
-                builder.close();
-                break;
-            }
-            case 2:
-                builder.add(Value(randomGenerator.mt() % 2 ? true : false));
-                break;
-            case 3:
-                addString(builder);
-                break;
-            case 4:
-                builder.add(Value(randomGenerator.mt()));
-                break;
-            case 5: {
-                double value1 = randomGenerator.mt();
-                double value2 = randomGenerator.mt();
-                double result = 0;
-                if (value2 != 0) {
-                    result = value1 / value2;
-                }
-                builder.add(Value(result));
-                break;
-            }
-            case 6:
-                builder.add(Value(ValueType::Null));
-                break;
-            case 7:
-                builder.add(Value(""));
-                break;
-            case 8:
-                builder.add(Value(int64_t(INT64_MIN)));
-                break;
-            case 9: {
-                int value = randomGenerator.mt();
-                if (value > 0) {
-                    value *= -1;
-                }
-                builder.add(Value(value));
-            }
-            default:
-                break;
+        builder.close();
+        break;
+      }
+      case 1: {
+        builder.openObject(randomGenerator.mt() % 2 ? true : false);
+        size_t numMembers = randomGenerator.mt() % 10;
+        for (size_t i = 0; i < numMembers; ++i) {
+          std::string key = "test" + std::to_string(i);
+          builder.add(Value(key));
+          generateVelocypack(builder, depth + 1);
         }
+        builder.close();
+        break;
+      }
+      case 2:
+        builder.add(Value(randomGenerator.mt() % 2 ? true : false));
+        break;
+      case 3:
+        addString(builder);
+        break;
+      case 4:
+        builder.add(Value(randomGenerator.mt()));
+        break;
+      case 5: {
+        double value1 = randomGenerator.mt();
+        double value2 = randomGenerator.mt();
+        double result = 0;
+        if (value2 != 0) {
+          result = value1 / value2;
+        }
+        builder.add(Value(result));
+        break;
+      }
+      case 6:
+        builder.add(Value(ValueType::Null));
+        break;
+      case 7:
+        builder.add(Value(""));
+        break;
+      case 8:
+        builder.add(Value(int64_t(INT64_MIN)));
+        break;
+      case 9: {
+        int value = randomGenerator.mt();
+        if (value > 0) {
+          value *= -1;
+        }
+        builder.add(Value(value));
+      }
+      default:
         break;
     }
+    break;
+  }
 }
 
 int main(int argc, char* argv[]) {
-    VELOCYPACK_GLOBAL_EXCEPTION_TRY
-        bool isTypeAssigned = false;
-        size_t iterations = 1;
-        Format format;
+  VELOCYPACK_GLOBAL_EXCEPTION_TRY
+    bool isTypeAssigned = false;
+    size_t iterations = 1;
+    Format format;
 
-        int i = 1;
-        while (i < argc) {
-            char const *p = argv[i];
-            if (isOption(p, "--help")) {
-                usage(argv);
-                return EXIT_SUCCESS;
-            } else if (isOption(p, "--vpack") && !isTypeAssigned) {
-                isTypeAssigned = true;
-                format = Format::VPACK;
-            } else if (isOption(p, "--json") && !isTypeAssigned) {
-                isTypeAssigned = true;
-                format = Format::JSON;
-            } else if (int value = atoi(p); value >= 1) {
-                iterations = value;
-            } else {
-                usage(argv);
-                return EXIT_FAILURE;
-            }
-            ++i;
-        }
+    int i = 1;
+    while (i < argc) {
+      char const *p = argv[i];
+      if (isOption(p, "--help")) {
+        usage(argv);
+        return EXIT_SUCCESS;
+      } else if (isOption(p, "--vpack") && !isTypeAssigned) {
+        isTypeAssigned = true;
+        format = Format::VPACK;
+      } else if (isOption(p, "--json") && !isTypeAssigned) {
+        isTypeAssigned = true;
+        format = Format::JSON;
+      } else if (int value = atoi(p); value >= 1) {
+        iterations = value;
+      } else {
+        usage(argv);
+        return EXIT_FAILURE;
+      }
+      ++i;
+    }
 
-        while (iterations-- > 0) {
-            Builder builder;
-            generateVelocypack(builder, 0);
+    while (iterations-- > 0) {
+      Builder builder;
+      generateVelocypack(builder, 0);
 
-            if (format == Format::JSON) {
-                Parser parser;
-                parser.parse(builder.slice().toJson());
-            } else {
-                Validator validator;
-                validator.validate(builder.slice().start(), builder.slice().byteSize());
-            }
-        }
-    VELOCYPACK_GLOBAL_EXCEPTION_CATCH
+      if (format == Format::JSON) {
+        Parser parser;
+        parser.parse(builder.slice().toJson());
+      } else {
+        Validator validator;
+        validator.validate(builder.slice().start(), builder.slice().byteSize());
+      }
+    }
+  VELOCYPACK_GLOBAL_EXCEPTION_CATCH
 }
