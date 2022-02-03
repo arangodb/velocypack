65dd7e4508a9249da17b74623d2ed7e752787fe2
cpjulia <julia@arangodb.com>
2022-02-03 13:02:39 -0300

    Updated fuzzer with parallelism

--- tools/fuzzer.cpp
+++ tools/fuzzer.cpp
@@ -25,11 +25,11 @@
 
 #include <iostream>
 #include <string>
-#include <memory>
 
 #include "velocypack/vpack.h"
 #include "velocypack/velocypack-exception-macros.h"
 #include <random>
+#include <thread>
 
 using namespace arangodb::velocypack;
 
@@ -40,14 +40,11 @@ enum class Format {
 enum RandomBuilderAdditions {ADD_ARRAY=0, ADD_OBJECT, ADD_BOOLEAN, ADD_STRING, ADD_NULL, ADD_UINT64, ADD_INT64, ADD_DOUBLE};
 
 struct RandomGenerator {
-  RandomGenerator() : mt{rd()}, mt64{rd()} {}
+  RandomGenerator() : mt64{rd()} {}
 
   std::random_device rd;
-  std::mt19937 mt;
   std::mt19937_64 mt64;
 };
-
-RandomGenerator randomGenerator;
 
 static void usage(char* argv[]) {
   std::cout << "Usage: " << argv[0] << " [OPTIONS] [ITERATIONS]"
@@ -56,6 +53,8 @@ static void usage(char* argv[]) {
             << std::endl;
   std::cout << "The amout of times it does this is supplied by <iterations>."
             << std::endl;
+  std::cout << "The paralelization is supplied by <threads>."
+            << std::endl;
   std::cout << "Available options are:" << std::endl;
   std::cout << " --vpack       create VPack."
             << std::endl;
@@ -63,49 +62,51 @@ static void usage(char* argv[]) {
             << std::endl;
   std::cout << " <iterations>  number of iterations. Default: 1"
             << std::endl;
+  std::cout << " <threads>  number of threads. Default: 1"
+            << std::endl;
 }
 
 static inline bool isOption(char const* arg, char const* expected) {
   return (strcmp(arg, expected) == 0);
 }
 
-static void addString(Builder& builder) {
+static void addString(Builder& builder, RandomGenerator& randomGenerator) {
   static auto &availableChars = "0123456789"
                                 "abcdefghijklmnopqrstuvwxyz"
                                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  size_t length = randomGenerator.mt() % 1000;
+  size_t length = randomGenerator.mt64() % 1000;
   std::string s;
   s.reserve(length);
   while (length--) {
-    s += availableChars[randomGenerator.mt() % (sizeof(availableChars) - 1)];
+    s += availableChars[randomGenerator.mt64() % (sizeof(availableChars) - 1)];
   }
   builder.add(Value(s));
 }
 
-static void generateVelocypack(Builder& builder, size_t depth) {
+static void generateVelocypack(Builder& builder, size_t depth, RandomGenerator& randomGenerator) {
   RandomBuilderAdditions randomBuilderAdds;
   while (true) {
-    randomBuilderAdds = static_cast<RandomBuilderAdditions>(randomGenerator.mt64() % 9);
+    randomBuilderAdds = static_cast<RandomBuilderAdditions>(randomGenerator.mt64() % 8);
     if (depth > 10 && randomBuilderAdds < 2) {
       continue;
     }
     switch (randomBuilderAdds) {
       case ADD_ARRAY: {
-        builder.openArray(randomGenerator.mt() % 2 ? true : false);
-        size_t numMembers = randomGenerator.mt() % 10;
+        builder.openArray(randomGenerator.mt64() % 2 ? true : false);
+        size_t numMembers = randomGenerator.mt64() % 10;
         for (size_t i = 0; i < numMembers; ++i) {
-          generateVelocypack(builder, depth + 1);
+          generateVelocypack(builder, depth + 1, randomGenerator);
         }
         builder.close();
         break;
       }
       case ADD_OBJECT: {
-        builder.openObject(randomGenerator.mt() % 2 ? true : false);
-        size_t numMembers = randomGenerator.mt() % 10;
+        builder.openObject(randomGenerator.mt64() % 2 ? true : false);
+        size_t numMembers = randomGenerator.mt64() % 10;
         for (size_t i = 0; i < numMembers; ++i) {
           std::string key = "test" + std::to_string(i);
           builder.add(Value(key));
-          generateVelocypack(builder, depth + 1);
+          generateVelocypack(builder, depth + 1, randomGenerator);
         }
         builder.close();
         break;
@@ -114,7 +115,7 @@ static void generateVelocypack(Builder& builder, size_t depth) {
         builder.add(Value(randomGenerator.mt64() % 2 ? true : false));
         break;
       case ADD_STRING:
-        addString(builder);
+        addString(builder, randomGenerator);
         break;
       case ADD_NULL:
         builder.add(Value(ValueType::Null));
@@ -125,11 +126,7 @@ static void generateVelocypack(Builder& builder, size_t depth) {
       case ADD_INT64: {
         uint64_t uintValue = randomGenerator.mt64();
         int64_t intValue;
-        if (uintValue <= std::numeric_limits<uint64_t>::max() ) {
-          intValue = uintValue;
-        } else {
-          intValue = 1; // to be changed
-        }
+        memcpy(&intValue, &uintValue, sizeof(uintValue));
         builder.add(Value(intValue));
         break;
       }
@@ -148,8 +145,10 @@ static void generateVelocypack(Builder& builder, size_t depth) {
 
 int main(int argc, char* argv[]) {
   VELOCYPACK_GLOBAL_EXCEPTION_TRY
+    std::vector<std::thread> threads;
     bool isTypeAssigned = false;
-    size_t iterations = 1;
+    size_t numIterations = 1;
+    size_t numThreads = 1;
     Format format;
 
     int i = 1;
@@ -165,7 +164,12 @@ int main(int argc, char* argv[]) {
         isTypeAssigned = true;
         format = Format::JSON;
       } else if (int value = atoi(p); value >= 1) {
-        iterations = value;
+        if (i == 3) { // as threads and iterations are optional arguments, threads must be preceeded by iterations
+                      // in the command line, hence, argument 3
+          numThreads = value;
+        } else {
+          numIterations = value;
+        }
       } else {
         usage(argv);
         return EXIT_FAILURE;
@@ -173,16 +177,36 @@ int main(int argc, char* argv[]) {
       ++i;
     }
 
-    while (iterations-- > 0) {
-      Builder builder;
-      generateVelocypack(builder, 0);
+    size_t itsPerThread = numIterations / numThreads;
+    size_t leftoverIts = numIterations % numThreads;
 
-      if (format == Format::JSON) {
-        Parser parser;
-        parser.parse(builder.slice().toJson());
-      } else {
-        Validator validator;
-        validator.validate(builder.slice().start(), builder.slice().byteSize());
+    auto threadCallback = [format] (size_t iterations) {
+      RandomGenerator randomGenerator;
+      while (iterations-- > 0) {
+        Builder builder;
+        generateVelocypack(builder, 0, randomGenerator);
+
+        if (format == Format::JSON) {
+          Parser parser;
+          parser.parse(builder.slice().toJson());
+        } else {
+          Validator validator;
+          validator.validate(builder.slice().start(), builder.slice().byteSize());
+        }
+      }
+    };
+
+    for (size_t i = 0; i < numThreads; ++i) {
+      size_t iterations = itsPerThread;
+      if (i == numThreads - 1) {
+        iterations += leftoverIts;
+      }
+      threads.emplace_back(std::thread(threadCallback, iterations));
+    }
+
+    for (auto &thread: threads) {
+      if (thread.joinable()) {
+        thread.join();
       }
     }
   VELOCYPACK_GLOBAL_EXCEPTION_CATCH
