2d0837a74ed4fe22179c4ea688ce33e87ac5aa91
Jan <jsteemann@users.noreply.github.com>
2022-10-19 14:48:52 +0200

    add size-constrained string sink (#135)

--- include/velocypack/Sink.h
+++ include/velocypack/Sink.h
@@ -48,89 +48,154 @@ struct Sink {
 
 template<typename T>
 struct ByteBufferSinkImpl final : public Sink {
-  explicit ByteBufferSinkImpl(Buffer<T>* buffer) : buffer(buffer) {}
+  explicit ByteBufferSinkImpl(Buffer<T>* buffer) : _buffer(buffer) {}
 
-  void push_back(char c) override final { buffer->push_back(c); }
+  void push_back(char c) override final { _buffer->push_back(c); }
 
   void append(std::string const& p) override final {
-    buffer->append(p.c_str(), p.size());
+    _buffer->append(p.c_str(), p.size());
   }
 
-  void append(char const* p) override final { buffer->append(p, strlen(p)); }
+  void append(char const* p) override final { _buffer->append(p, strlen(p)); }
 
   void append(char const* p, ValueLength len) override final {
-    buffer->append(p, len);
+    _buffer->append(p, len);
   }
 
-  void reserve(ValueLength len) override final { buffer->reserve(len); }
+  void reserve(ValueLength len) override final { _buffer->reserve(len); }
 
-  Buffer<T>* buffer;
+ private:
+  Buffer<T>* _buffer;
 };
 
 typedef ByteBufferSinkImpl<char> CharBufferSink;
 
 template<typename T>
 struct StringSinkImpl final : public Sink {
-  explicit StringSinkImpl(T* buffer) : buffer(buffer) {}
+  explicit StringSinkImpl(T* buffer) : _buffer(buffer) {}
 
-  void push_back(char c) override final { buffer->push_back(c); }
+  void push_back(char c) override final { _buffer->push_back(c); }
 
-  void append(std::string const& p) override final { buffer->append(p); }
+  void append(std::string const& p) override final { _buffer->append(p); }
 
-  void append(char const* p) override final { buffer->append(p, strlen(p)); }
+  void append(char const* p) override final { _buffer->append(p, strlen(p)); }
 
   void append(char const* p, ValueLength len) override final {
-    buffer->append(p, checkOverflow(len));
+    _buffer->append(p, checkOverflow(len));
   }
 
   void reserve(ValueLength len) override final {
-    ValueLength length = len + buffer->size();
-    if (length <= buffer->capacity()) {
+    ValueLength length = len + _buffer->size();
+    if (length <= _buffer->capacity()) {
       return;
     }
-    buffer->reserve(checkOverflow(length));
+    _buffer->reserve(checkOverflow(len));
   }
 
-  T* buffer;
+ private:
+  T* _buffer;
 };
 
 typedef StringSinkImpl<std::string> StringSink;
 
+// a sink with an upper bound for the generated output value
+template<typename T>
+struct SizeConstrainedStringSinkImpl final : public Sink {
+  explicit SizeConstrainedStringSinkImpl(T* buffer, ValueLength maxLength) 
+      : _buffer(buffer), _maxLength(maxLength), _overflowed(false) {}
+
+  void push_back(char c) override final { 
+    if (_buffer->size() < _maxLength) {
+      _buffer->push_back(c); 
+    } else {
+      _overflowed = true;
+    }
+  }
+
+  void append(std::string const& p) override final {
+    append(p.data(), p.size());
+  }
+
+  void append(char const* p) override final { append(p, strlen(p)); }
+
+  void append(char const* p, ValueLength len) override final {
+    if (_buffer->size() < _maxLength) {
+      ValueLength total = checkOverflow(_buffer->size() + checkOverflow(len));
+      if (total <= _maxLength) {
+        _buffer->append(p, len);
+        return;
+      }
+      ValueLength left = _maxLength - _buffer->size();
+      if (len > left) {
+        len = left;
+      }
+      _buffer->append(p, len);
+    }
+    _overflowed = true;
+  }
+
+  void reserve(ValueLength len) override final {
+    ValueLength total = checkOverflow(_buffer->size() + checkOverflow(len));
+    if (total <= _buffer->capacity()) {
+      return;
+    }
+    ValueLength left = _maxLength - _buffer->size();
+    if (len > left) {
+      len = left;
+    }
+    _buffer->reserve(checkOverflow(len));
+  }
+
+  bool overflowed() const noexcept { return _overflowed; }
+
+ private:
+  T* _buffer;
+  std::size_t const _maxLength;
+  bool _overflowed;
+};
+
+typedef SizeConstrainedStringSinkImpl<std::string> SizeConstrainedStringSink;
+
+// only tracks the length of the generated output
 struct StringLengthSink final : public Sink {
-  StringLengthSink() : length(0) {}
+  StringLengthSink() : _length(0) {}
 
-  void push_back(char) override final { ++length; }
+  void push_back(char) override final { ++_length; }
 
-  void append(std::string const& p) override final { length += p.size(); }
+  void append(std::string const& p) override final { _length += p.size(); }
 
-  void append(char const* p) override final { length += strlen(p); }
+  void append(char const* p) override final { _length += strlen(p); }
 
-  void append(char const*, ValueLength len) override final { length += len; }
+  void append(char const*, ValueLength len) override final { _length += len; }
 
   void reserve(ValueLength) override final {}
 
-  ValueLength length;
+  ValueLength length() const noexcept { return _length; }
+
+ private:
+  ValueLength _length;
 };
 
 template<typename T>
 struct StreamSinkImpl final : public Sink {
-  explicit StreamSinkImpl(T* stream) : stream(stream) {}
+  explicit StreamSinkImpl(T* stream) : _stream(stream) {}
 
-  void push_back(char c) override final { *stream << c; }
+  void push_back(char c) override final { *_stream << c; }
 
-  void append(std::string const& p) override final { *stream << p; }
+  void append(std::string const& p) override final { *_stream << p; }
 
   void append(char const* p) override final {
-    stream->write(p, static_cast<std::streamsize>(strlen(p)));
+    _stream->write(p, static_cast<std::streamsize>(strlen(p)));
   }
 
   void append(char const* p, ValueLength len) override final {
-    stream->write(p, static_cast<std::streamsize>(len));
+    _stream->write(p, static_cast<std::streamsize>(len));
   }
 
   void reserve(ValueLength) override final {}
 
-  T* stream;
+ private:
+  T* _stream;
 };
 
 typedef StreamSinkImpl<std::ostringstream> StringStreamSink;
@@ -141,6 +206,7 @@ typedef StreamSinkImpl<std::ofstream> OutputFileStreamSink;
 using VPackSink = arangodb::velocypack::Sink;
 using VPackCharBufferSink = arangodb::velocypack::CharBufferSink;
 using VPackStringSink = arangodb::velocypack::StringSink;
+using VPackSizeConstrainedStringSink = arangodb::velocypack::SizeConstrainedStringSink;
 using VPackStringLengthSink = arangodb::velocypack::StringLengthSink;
 using VPackStringStreamSink = arangodb::velocypack::StringStreamSink;
 using VPackOutputFileStreamSink = arangodb::velocypack::OutputFileStreamSink;
--- tests/testsSink.cpp
+++ tests/testsSink.cpp
@@ -77,20 +77,154 @@ TEST(SinkTest, StringSink) {
   ASSERT_EQ("foobarbaz", out);
 }
 
+TEST(SinkTest, SizeConstrainedStringSinkAlwaysEmpty) {
+  std::string out;
+  SizeConstrainedStringSink s(&out, 0);
+
+  ASSERT_TRUE(out.empty());
+  ASSERT_FALSE(s.overflowed());
+
+  s.push_back('x');
+  ASSERT_TRUE(out.empty());
+  ASSERT_TRUE(s.overflowed());
+
+  s.append("foobarbaz");
+  ASSERT_TRUE(out.empty());
+  ASSERT_TRUE(s.overflowed());
+  
+  s.append("123", 3);
+  ASSERT_TRUE(out.empty());
+  ASSERT_TRUE(s.overflowed());
+}
+
+TEST(SinkTest, SizeConstrainedStringSinkSmall) {
+  std::string out;
+  SizeConstrainedStringSink s(&out, 15);
+
+  ASSERT_TRUE(out.empty());
+  ASSERT_FALSE(s.overflowed());
+
+  s.push_back('x');
+  ASSERT_EQ("x", out);
+  ASSERT_FALSE(s.overflowed());
+
+  s.append("foobarbaz");
+  ASSERT_EQ("xfoobarbaz", out);
+  ASSERT_FALSE(s.overflowed());
+  
+  s.append("123", 3);
+  ASSERT_EQ("xfoobarbaz123", out);
+  ASSERT_FALSE(s.overflowed());
+  
+  s.push_back('y');
+  ASSERT_EQ("xfoobarbaz123y", out);
+  ASSERT_FALSE(s.overflowed());
+  
+  s.append("123", 3);
+  ASSERT_EQ("xfoobarbaz123y1", out);
+  ASSERT_TRUE(s.overflowed());
+}
+
+TEST(SinkTest, SizeConstrainedStringSinkLarger) {
+  std::string out;
+  SizeConstrainedStringSink s(&out, 2048);
+
+  ASSERT_TRUE(out.empty());
+  ASSERT_FALSE(s.overflowed());
+
+  for (std::size_t i = 0; i < 4096; ++i) {
+    s.push_back('x');
+    if (i >= 2048) {
+      ASSERT_EQ(2048, out.size());
+      ASSERT_TRUE(s.overflowed());
+    } else {
+      ASSERT_EQ(i + 1, out.size());
+      ASSERT_FALSE(s.overflowed());
+    }
+  }
+}
+
+TEST(SinkTest, SizeConstrainedStringSinkLongStringAppend) {
+  std::string out;
+  SizeConstrainedStringSink s(&out, 2092);
+
+  ASSERT_TRUE(out.empty());
+  ASSERT_FALSE(s.overflowed());
+
+  s.append("meow");
+  ASSERT_EQ(4, out.size());
+  ASSERT_FALSE(s.overflowed());
+
+  std::string append(16384, 'x');
+  s.append(append);
+  ASSERT_EQ(2092, out.size());
+  ASSERT_EQ(std::string("meow") + append.substr(0, 2088), out);
+  ASSERT_TRUE(s.overflowed());
+}
+  
+TEST(SinkTest, SizeConstrainedStringSinkReserve) {
+  {
+    std::string out;
+    SizeConstrainedStringSink s(&out, 0);
+    
+    std::size_t oldCapacity = out.capacity();
+    // should do nothing
+    s.reserve(10);
+    ASSERT_EQ(oldCapacity, out.capacity());
+    
+    s.reserve(128);
+    ASSERT_EQ(oldCapacity, out.capacity());
+    
+    s.reserve(4096);
+    ASSERT_EQ(oldCapacity, out.capacity());
+  }
+  
+  {
+    std::string out;
+    SizeConstrainedStringSink s(&out, 4096);
+    
+    // should do something. however, we don't know
+    // the exact capacity, as it depends on the internals
+    // of std::string
+    s.reserve(128);
+    ASSERT_GE(out.capacity(), 128);
+    
+    std::size_t oldCapacity = out.capacity();
+    // should not do anything
+    s.reserve(128);
+    ASSERT_EQ(oldCapacity, out.capacity());
+    
+    s.reserve(256);
+    ASSERT_GE(out.capacity(), 256);
+    
+    s.reserve(4096);
+    ASSERT_GE(out.capacity(), 4096);
+
+    oldCapacity = out.capacity();
+    for (std::size_t i = 0; i < 10; ++i) {
+      s.reserve(128);
+      // capacity should not have changed
+      ASSERT_EQ(oldCapacity, out.capacity());
+    }
+  }
+}
+
 TEST(SinkTest, StringLengthSink) {
   StringLengthSink s;
 
+  ASSERT_EQ(0, s.length());
+
   s.push_back('x');
-  ASSERT_EQ(1, s.length);
+  ASSERT_EQ(1, s.length());
 
   s.append(std::string("foobarbaz"));
-  ASSERT_EQ(10, s.length);
+  ASSERT_EQ(10, s.length());
 
   s.append("foobarbaz");
-  ASSERT_EQ(19, s.length);
+  ASSERT_EQ(19, s.length());
 
   s.append("foobarbaz", 9);
-  ASSERT_EQ(28, s.length);
+  ASSERT_EQ(28, s.length());
 }
 
 TEST(SinkTest, StringStreamSink) {
