28604f87446ab3140924aac68af2d30b9e58f05a
Max Neunhoeffer <max@arangodb.com>
2024-09-26 15:06:05 +0200

    Restrict to large numbers.

--- src/Dumper.cpp
+++ src/Dumper.cpp
@@ -217,6 +217,9 @@ void Dumper::appendDouble(double v) {
   char temp[24];
   int len = fpconv_dtoa(v, &temp[0]);
   _sink->append(&temp[0], static_cast<ValueLength>(len));
+  if (fabs(v) < ldexpl(1.0, 53)) {
+    return;
+  }
   for (size_t i = 0; i < len; ++i) {
     if (temp[i] == '.') {
       return;
